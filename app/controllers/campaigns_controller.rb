4e562e37663feada9aa6b2af0c29a31dac794c23
Whapow <joshuavancamp@gmail.com>
2018-06-10 22:41:24 -0400

    add campaigns controller and routes

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,17 @@
+class CampaignsController < ApplicationController
+  before_action :set_campaign, except: [:index]
+
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_campaign
+    @campaign = Campaign.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Campaigns
+  get 'campaigns', to: 'campaigns#index'
+  post 'campaigns', to: 'campaigns#create'
+  get 'campaigns/:id', to: 'campaigns#show'
+  patch 'campaigns/:id', to: 'campaigns#update'
+  delete 'campaigns/:id', to: 'campaigns#destroy'
+
 end
