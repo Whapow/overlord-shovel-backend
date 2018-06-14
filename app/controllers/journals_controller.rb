606383d9a45773bde97100b3d389a29111c38f80
Whapow <joshuavancamp@gmail.com>
2018-06-13 22:28:54 -0400

    add CRUD for journals

---
+++ app/controllers/journals_controller.rb
@@ -0,0 +1,47 @@
+class JournalsController < ApplicationController
+  before_action :set_journal, except: [:index, :create]
+
+  def index
+    @journals = Journal.where(campaign_id: params[:id])
+    serialize(@journals)
+  end
+
+  def create
+    @journal = Journal.new(journal_params)
+    if @journal.save
+      serialize(@journal)
+    else
+      render json: @journal.errors
+    end
+  end
+
+  def update
+    if @journal.update_attributes(journal_params)
+      serialize(@journal)
+    else
+      render json: @journal.errors
+    end
+  end
+
+  def destroy 
+    if @journal.destroy
+      render json: {status: 202, message: 'deleted'}
+    else
+      render json: @journal.errors
+    end
+  end
+
+  private 
+
+  def set_journal
+    @journal = Journal.find(params[:id])
+  end
+
+  def serialize(target)
+    render json: JournalSerializer.new(target)
+  end
+
+  def journal_params
+    params.require(:journal).permit(:name, :campaign_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,24 @@ Rails.application.routes.draw do
   patch 'campaigns/:id', to: 'campaigns#update'
   delete 'campaigns/:id', to: 'campaigns#destroy'
 
-  # Items
-  get 'campaigns/:id/items', to: 'items#index'
-  post 'items', to: 'items#create'
-  patch 'items/:id', to: 'items#update'
-  delete 'items/:id', to: 'items#destroy'
-
   # Characters
   get 'campaigns/:id/characters', to: 'characters#index'
   get 'characters', to: 'characters#index'
   post 'characters', to: 'characters#create'
   patch 'characters/:id', to: 'characters#update'
   delete 'characters/:id', to: 'characters#destroy'
+  
+  # Items
+  get 'campaigns/:id/items', to: 'items#index'
+  post 'items', to: 'items#create'
+  patch 'items/:id', to: 'items#update'
+  delete 'items/:id', to: 'items#destroy'
+
+  # Journals
+  get 'campaigns/:id/journals', to: 'journals#index'
+  post 'journals', to: 'journals#create'
+  patch 'journals/:id', to: 'journals#update'
+  delete 'journals/:id', to: 'journals#destroy'
+
 
 end
