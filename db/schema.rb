f986d378e5393c9692dd41eb6c9e24a592412732
Whapow <joshuavancamp@gmail.com>
2018-06-13 22:28:33 -0400

    add soft delete to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_013539) do
+ActiveRecord::Schema.define(version: 2018_06_14_022631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2018_06_11_013539) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "deleted"
   end
 
 end
