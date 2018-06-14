f1da7190927eb6071f941cf37e43da7b31c3ea81
Whapow <joshuavancamp@gmail.com>
2018-06-14 15:42:41 -0400

    annotate entry serializer

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: entries
+#
+#  journal_id   :bigint(8)        not null
+#  character_id :bigint(8)        not null
+#  experience   :integer
+#  reward       :decimal(, )
+#  discarded_at :datetime
+#
+
 class EntrySerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :journal_id, :character_id, :reward, :experience
