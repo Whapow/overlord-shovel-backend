344c12fc75964bea5b13c5b8b5bf39380155c884
Whapow <joshuavancamp@gmail.com>
2018-06-14 20:45:41 -0400

    update seeds.rb to conform to relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+users = [
+  { id: 1, name: 'Joshua', email: 'Joshua@overlord-shovel.com' },
+  { id: 2, name: 'Jessica', email: 'Jessica@overlord-shovel.com' },
+  { id: 3, name: 'Evan', email: 'Evan@overlord-shovel.com' },
+  { id: 4, name: 'Weston', email: 'Weston@overlord-shovel.com' }
+]
+users.each{|user| User.create!(user)}
+
 campaigns = [
   { id: 1, gm_id: 1, name: 'Cy\'s Saga' },
   { id: 2, gm_id: 1, name: 'As The Seasons Change' },
   { id: 3, gm_id: 4, name: 'Farrenmore' },
   { id: 4, gm_id: 3, name: 'Sille' }
 ]
-campaigns.each{|campaign| Campaign.create(campaign)}
-
-users = [
-  { id: 1, name: 'Joshua' },
-  { id: 2, name: 'Jessica' },
-  { id: 3, name: 'Evan' },
-  { id: 4, name: 'Weston' }
-]
-users.each{|user| User.create(user)}
+campaigns.each{|campaign| Campaign.create!(campaign)}
 
 characters = [
   { id: 1, player_id: 1, campaign_id: 3, name: 'Audrael' },
   { id: 2, player_id: 2, campaign_id: 3, name: 'Mouse' },
   { id: 3, player_id: 3, campaign_id: 3, name: 'Sticky' }
 ]
-characters.each{|character| Character.create(character)}
+characters.each{|character| Character.create!(character)}
 
 items = [
   { id: 1, character_id: 1, campaign_id: 3, name: 'Diocles', description: 'Adamantine Gladius', value: 500 },
@@ -41,7 +41,7 @@ items = [
   { id: 14, character_id: nil, campaign_id: 2, name: 'Random Thing 03', description: 'Probably Junk', value: 0 },
   { id: 15, character_id: nil, campaign_id: 2, name: 'Random Thing 04', description: 'Probably Junk', value: 0 }
 ]
-items.each{|item| Item.create(item)}
+items.each{|item| Item.create!(item)}
 
 journals = [
   { id: 1, name: 'Journal 01', campaign_id: 3 },
@@ -49,4 +49,8 @@ journals = [
   { id: 3, name: 'Journal 03', campaign_id: 1 },
   { id: 4, name: 'Journal 04', campaign_id: 2 }
 ]
-journals.each{|journal| Journal.create(journal)}
+journals.each{|journal| Journal.create!(journal)}
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
