6d1c17437d24edc6fe9cd93c325459e6ca06bd66
Whapow <joshuavancamp@gmail.com>
2018-06-16 15:16:44 -0400

    add CRUD for camapigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,13 +1,38 @@
 class CampaignsController < ApplicationController
-  before_action :set_campaign, except: [:index]
+  before_action :set_campaign, except: [:index, :create]
 
   def index
-    @campaigns = Campaign.all
+    @campaigns = Campaign.kept
     serialize @campaigns
   end
 
   def show
     serialize @campaign
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+    if @campaign.save
+      serialize(@campaign)
+    else
+      render json: @campaign.errors
+    end
+  end
+
+  def update
+    if @campaign.update_attributes(campaign_params)
+      serialize(@campaign)
+    else
+      render json: @campaign.errors
+    end
+  end
+
+  def destroy
+    if @campaign.discard
+      render json: {status: 202, message: 'deleted'}
+    else
+      render json: @campaign.errors
+    end
   end
 
   private
@@ -18,6 +43,10 @@ class CampaignsController < ApplicationController
 
   def serialize(target)
     render json: CampaignSerializer.new(target)
+  end
+
+  def campaign_params
+    params.require(:campaign).permit(:name, :gm_id)
   end
 
 end
