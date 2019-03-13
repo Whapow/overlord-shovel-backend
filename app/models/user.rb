1ce7aa7b12bc21a0078f64003e79e16ccefeec68
Whapow <joshuavancamp@gmail.com>
2019-03-13 12:04:47 -0400

    fix user relationships with foreign keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_one :session
-  has_many :characters
-  has_many :campaigns
+  has_many :characters, foreign_key: :player_id
+  has_many :campaigns, foreign_key: :gm_id
 
   validates_presence_of :email, :username, :password_digest
   validates_uniqueness_of :username, :email
